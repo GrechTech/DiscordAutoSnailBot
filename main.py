cd0ba1b925a33a63f39c9b24cb5ecba781bb69cc
GrechTech <thomas@grechtech.co.uk>
2022-03-17 22:05:22 +0000

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,6 @@ URLS_PATH = os.path.join(DIR_PATH,"urls.txt")
 
 # Auto Snail find URL
 def FindURL(string):
-    # findall() has been used 
-    # with valid conditions for urls in string
     regex = r"(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'\".,<>?«»“”‘’]))"
     url = re.findall(regex,string)      
     return [x[0] for x in url]
